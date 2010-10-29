513b75e42e0a20188911074debdc3e44fb80c59b
feiyinzilgd <feiyinzilgd@sina.com>
2010-10-29 05:34:47 +0000

    update the server code

---
+++  qtremotedroid/src/leftbuttonmethod.h
@@ -0,0 +1,12 @@
+#ifndef LEFTBUTTONMETHOD_H
+#define LEFTBUTTONMETHOD_H
+
+#include "oscservermethod.h"
+
+class LeftButtonMethod : public OscServerMethod
+{
+public:
+    //LeftButtonMethod();
+};
+
+#endif // LEFTBUTTONMETHOD_H
---
+++  qtremotedroid/src/rightbutton.h
@@ -0,0 +1,12 @@
+#ifndef RIGHTBUTTON_H
+#define RIGHTBUTTON_H
+
+#include "oscservermethod.h"
+
+class RightButton : public OscServerMethod
+{
+public:
+
+};
+
+#endif // RIGHTBUTTON_H
