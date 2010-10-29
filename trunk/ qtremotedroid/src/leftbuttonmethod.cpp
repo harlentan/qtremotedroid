e2e32c6ce2381178f82f62619641fade98a980f1
feiyinzilgd@sina.com <feiyinzilgd@sina.com@ef9c3d9b-d8cc-bc40-d932-06c4964f01bb>
2010-10-29 05:34:47 +0000

    update the server code

---
+++ trunk/ qtremotedroid/src/leftbuttonmethod.cpp
@@ -0,0 +1,5 @@
+#include "leftbuttonmethod.h"
+
+LeftButtonMethod::LeftButtonMethod()
+{
+}
---
+++ trunk/ qtremotedroid/src/oscserver.h
@@ -0,0 +1,12 @@
+#ifndef OSCSERVER_H
+#define OSCSERVER_H
+
+#include "OSC_lgd/WOscReceiver.h"
+
+class OscServer : public WOscReceiver
+{
+public:
+    OscServer();
+};
+
+#endif // OSCSERVER_H
