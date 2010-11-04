c5d32e2769e25c3b0f7cecb5e10b543c2fc5eac6
feiyinzilgd@sina.com <feiyinzilgd@sina.com@ef9c3d9b-d8cc-bc40-d932-06c4964f01bb>
2010-11-04 02:36:21 +0000

    add the config.h file

---
+++ trunk/ qtremotedroid/QtRemoteDroidServer/src/config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define RD_VERSION "0.45"
+#define RD_BASE "Qt 4.7"
+#define RD_BUILD_TIME "14:41:13 3/11/2010"
+#define RD_HISTORY "Version 0.1"
+#define RD_DESCRIPTION "QtRemoteDroid is a wireless mouse software under LGPL"
+#define RD_COPYRIGHT "QtRemoteDroid org of Teleca"
+#define RD_NAME "QtRemoteDroid"
+
+#endif // CONFIG_H
