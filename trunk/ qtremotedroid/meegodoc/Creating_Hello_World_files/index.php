bf00b8c3dd9b130bea0ab5b7c8aea4b110dcc002
feiyinzilgd@sina.com <feiyinzilgd@sina.com@ef9c3d9b-d8cc-bc40-d932-06c4964f01bb>
2010-12-12 09:55:39 +0000

    delete my personal files

--- trunk/ qtremotedroid/meegodoc/Creating_Hello_World_files/index.php
+++
@@ -1,8 +0,0 @@
-/* generated javascript */
-var skin = 'meego';
-var stylepath = '/skins';
-
-/* MediaWiki:Common.js */
-/* Any JavaScript here will be loaded for all users on every page load. */
-
-/* MediaWiki:Meego.js */
